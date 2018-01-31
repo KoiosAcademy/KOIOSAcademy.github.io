8935e9aaaead7883f11c427f8d4def9d6cad5304
KoiosAcademy <35716803+KoiosAcademy@users.noreply.github.com>
2018-01-31 02:02:26 -0500

    Update views.py

--- app/views.py
+++ app/views.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# @Author: Alvin Lawson
-# @Date:   2015-05-11 12:40:10
-# @Last Modified by:   Alvin Lawson
-# @Last Modified time: 2015-05-11 13:12:49
+# @Author: GreenJedi
+# @Date:   2018-01-31 12:40:10
+# @Last Modified by:   JJLasher
+# @Last Modified time: 2018-01-31 13:12:49
 from flask import render_template
 from app import app
 
